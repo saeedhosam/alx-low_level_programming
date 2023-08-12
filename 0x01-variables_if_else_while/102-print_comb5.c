ef8822b37e8110172db79b86f4302662601c16a4
saeedhosam <makymaky757@gmail.com>
2023-08-12 21:33:41 +0300

    102.4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,18 +11,18 @@ int main(void)
 	int j;
 
 	for (i = 0; i <= 98; i++)
-		for (j = 0; j <= 99; j++)
-			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
-				putchar(' ');
-				putchar(j / 10 + '0');
-				putchar(j % 10 + '0');
-				if (i == 98)
-					continue;
-				putchar(',');
-				putchar(' ');
-			}
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 98)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	putchar('\n');
 	return (0);
 }
