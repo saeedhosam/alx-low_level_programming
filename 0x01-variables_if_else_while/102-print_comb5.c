c0280384d257a5eb069c62181d98fbaaffcd9abd
saeedhosam <makymaky757@gmail.com>
2023-08-12 21:26:36 +0300

    102.3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,25 +9,20 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l = 0;
-	const int n = 9;
 
 	for (i = 0; i <= n; i++)
 		for (j = 0; j <= n; j++)
-			for (k = i; k <= n; k++)
-				for (l = 1; l <= n; l++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					if (i == 9 && j == 8 && k == 9 && l == 9)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i == 98)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 	putchar('\n');
 	return (0);
 }
