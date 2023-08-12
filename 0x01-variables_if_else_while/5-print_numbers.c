a2d5c5fd062f76604c77934dffa2c2aed94b0254
saeedhosam <makymaky757@gmail.com>
2023-08-12 05:24:34 +0300

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers of base 10 that end with newline
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%i", i);
+	printf("\n");
+}
