b74623e5aab4e014f86ee473a689972645391c3e
saeedhosam <makymaky757@gmail.com>
2023-08-12 04:40:46 +0300

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - convert decimal into chars like in the ASCI II table
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char a
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
