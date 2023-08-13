691cfd49b603113a5fc25770287357cacafc890f
saeedhosam <makymaky757@gmail.com>
2023-08-13 06:42:11 +0300

    102

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - trying to output a text without using printf or puts
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
