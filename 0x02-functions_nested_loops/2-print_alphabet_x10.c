5e0c66662cf9b00e10edb690f96ca08ca800d34e
saeedhosam <makymaky757@gmail.com>
2023-08-16 15:29:12 +0300

    2.3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    for (j = 0; j <= 10; j++)
-    {
-        for (i = 49; i < 75; i++)
-            _putchar(i + '0');
-        _putchar('\n');
-    }
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 49; i < 75; i++)
+			_putchar(i + '0');
+		_putchar('\n');
+	}
 }
