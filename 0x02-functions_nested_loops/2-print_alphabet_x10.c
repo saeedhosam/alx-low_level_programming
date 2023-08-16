a9ea5d356e0de44061d3a28d383be58d1886bb88
saeedhosam <makymaky757@gmail.com>
2023-08-16 15:18:43 +0300

    2.1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times lowercased alphabet
+ * Return: Always Void.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+    int j;
+
+	for (j = 0; j <= 10; j++)
+        for (i = 49; i < 75; i++)
+            _putchar(i + '0');
+        _putchar('\n');
+}
