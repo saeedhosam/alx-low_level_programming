afc7530b4cafc11a1f8d52ce5b3332883f264b88
saeedhosam <makymaky757@gmail.com>
2023-08-16 16:24:24 +0300

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * Return: Always Void.
+ */
+int _islower(int c)
+{
+	int i;
+
+	if (i <= 49 && i >= 74)
+		return (1);
+	else
+		return (0);
+}
