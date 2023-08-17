4498c03d4b007237bb884f60dfcd1519004a756b
saeedhosam <makymaky757@gmail.com>
2023-08-18 00:33:59 +0300

    11 after several hours :')

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: from where we start ISA
+ * Return: int.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	if (n >= 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	putchar('\n');
+}
