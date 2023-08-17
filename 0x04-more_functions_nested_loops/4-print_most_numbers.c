056402d50fab1b2261daafa632ae5c6d411f5ae5
saeedhosam <makymaky757@gmail.com>
2023-08-18 01:50:05 +0300

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * Return: Void.
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
