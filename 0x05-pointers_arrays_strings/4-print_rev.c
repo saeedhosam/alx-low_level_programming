1c04774df86b8d8c7d51f1457fd84d5d11c0b2ed
saeedhosam <makymaky757@gmail.com>
2023-08-21 17:40:49 +0300

    4.5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,8 +11,7 @@ void print_rev(char *s)
 
 	for (i = 0; *(s + i) != '\0';)
 		i++;
-	for (j = i; j >= 0; j--)
-		if (j < 0)
-			_putchar(*(s + j));
+	for (j = i - 1; j >= 0; j--)
+		_putchar(*(s + j));
 	_putchar('\n');
 }
