7e0a11f5c9bcad0e16d4cd164e63ed13b8800183
saeedhosam <makymaky757@gmail.com>
2023-08-23 01:01:02 +0300

    100.6

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,15 +7,14 @@
 */
 int _atoi(char *s)
 {
-	int i, j = 0, k = 1;
+	int i, k = 1;
+	unsigned int j = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 48 && s[i] <= 57)
 		{
-			j = j * 10;
-			if (j != 2147483640)
-				j = j + (s[i] - '0');
+			j = j * 10 + (s[i] - '0');
 		}
 		else if (s[i] == 45)
 			k = k * -1;
@@ -24,8 +23,5 @@ int _atoi(char *s)
 		else if (j != 0)
 			break;
 	}
-	if (j == 2147483640)
-		return (k * j + k * 8);
-	else
-		return (k * j);
+	return (k * j);
 }
