a48e081d3af6033e357a53f6bd36a745199d3c4e
saeedhosam <makymaky757@gmail.com>
2023-08-22 17:45:39 +0300

    8.1

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,13 @@
 */
 void print_array(int *a, int n)
 {
-	for (n = 0; a[n] != '\0'; n++)
+	int i;
+	
+	for (i = 0; i < n; i++)
 	{
-		if (n != 0)
+		if (i != 0)
 			printf(", ");
-		printf("%d", a[n]);
+		printf("%d", a[i]);
 	}
 	printf("\n");
 }
