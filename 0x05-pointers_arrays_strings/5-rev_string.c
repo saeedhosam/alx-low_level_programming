dcb14303c073df6aae5d44c1f23b505c0aba0357
saeedhosam <makymaky757@gmail.com>
2023-08-22 11:05:14 +0300

    5.1m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * rev_string - a function that prints a string in reverse
+ * @s: the string's characters
+ * Return: nothing.
+*/
+void rev_string(char *s)
+{
+	int i, j;
+	char k;
+
+	for (i = 0; *(s + i) != '\0';)
+		i++;
+	for (j = 0; j < (i / 2); j++)
+	{
+		k = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = k;
+	}
+	printf("%s\n", s);
+}
