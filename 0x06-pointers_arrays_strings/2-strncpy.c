e145ca51281fe7b9e90e31eedd8990b489d1af5a
saeedhosam <makymaky757@gmail.com>
2023-08-23 20:12:08 +0300

    2.3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
-	if (i < n)
+	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
 }
