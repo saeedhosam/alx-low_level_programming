f072b3654263fa675c89426dbb71c67303b19b4e
saeedhosam <makymaky757@gmail.com>
2023-08-23 10:31:41 +0300

    1

---
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
+/*
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-strncat.c -o 1-strncat
+Hello 
+World!
+Hello W
+World!
+Hello W
+Hello WWorld!
+World!
+Hello WWorld!
+*/
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: the first string
+ * @src: the second string that's to be appended
+ * @n: maximum n bytes to take from src
+ * Return: a pointer to the resulting string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0';)
+		i++;
+	for (j = 0; j < n; j++, i++)
+	{
+		if (n >= 98)
+			n = 97;
+		dest[i] = src[j];
+	}
+	return (dest);
+}
