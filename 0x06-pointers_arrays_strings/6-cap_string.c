a7b3379c565a84e85ee22b67b7c2c3bb9afa0d98
saeedhosam <makymaky757@gmail.com>
2023-08-24 08:33:27 +0300

    6.4

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,14 +15,17 @@ char *cap_string(char *str)
 		{
 			if (str[i - 1] == 32 || str[i - 1] == 46 || str[i - 1] == 10)
 				str[i] = str[i] - 32;
-			else if (str[i - 1] == 9 || str[i - 1] == 44 || str[i - 1] == 59)
+			else if (str[i - 1] == 125 || str[i - 1] == 44 || str[i - 1] == 59)
 				str[i] = str[i] - 32;
 			else if (str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34)
 				str[i] = str[i] - 32;
 			else if (str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123)
 				str[i] = str[i] - 32;
-			else if (str[i - 1] == 125)
+			else if (str[i - 1] == 9)
+			{
 				str[i] = str[i] - 32;
+				str[i - 1] = 32;
+			}
 		}
 	}
 	return (str);
