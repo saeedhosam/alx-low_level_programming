4a12246a83af86b43b9165eae79b75d3c9eaec29
saeedhosam <makymaky757@gmail.com>
2023-08-24 08:30:50 +0300

    6.3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,19 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == 32)
-			str[i] = str[i] - 32;
-		else if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == 46)
-			str[i] = str[i] - 32;
-		else if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == 10)
-			str[i] = str[i] - 32;
-		else if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == 9)
-			str[i] = str[i] - 32;
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (str[i - 1] == 32 || str[i - 1] == 46 || str[i - 1] == 10)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] == 9 || str[i - 1] == 44 || str[i - 1] == 59)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] == 125)
+				str[i] = str[i] - 32;
+		}
 	}
 	return (str);
 }
