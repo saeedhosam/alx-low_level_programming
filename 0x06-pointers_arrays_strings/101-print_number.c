416cce67ad282acaf7480aa36b434a540ae71c3d
saeedhosam <makymaky757@gmail.com>
2023-08-24 21:07:02 +0300

    testt

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,20 +9,17 @@
 */
 void print_number(int n)
 {
-	int i = 0;
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    while (n != 0) {
-        i = i * 10 + n % 10;
-        n /= 10;
-    }
-    if (i == 0) {
-        _putchar('0');
-    }
-    while (i != 0) {
-        _putchar(i % 10 + '0');
-        i /= 10;
-    }
+	unsigned int i = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar('0');
+	for (; n != 0; n = n / 10)
+		i = i * 10 + n % 10;
+	for (; i != 0; i = i / 10)
+		_putchar(i % 10 + '0');
 }
