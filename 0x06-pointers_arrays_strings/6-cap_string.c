562701d91b0cceade7fd01bbc5cd2396752e9c85
saeedhosam <makymaky757@gmail.com>
2023-08-24 09:17:39 +0300

    6.10

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,8 @@ char *cap_string(char *str)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i - 1] == ' '
+			if (i == 0
+				||str[i - 1] == ' '
 				|| str[i - 1] == '\t'
 				|| str[i - 1] == '\n'
 				|| str[i - 1] == ','
