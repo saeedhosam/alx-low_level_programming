d2a11a78e6fc111e4ea9d2930a2869c5fc34b094
saeedhosam <makymaky757@gmail.com>
2023-08-25 11:10:34 +0300

    101.1.2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,20 +9,35 @@
 */
 void print_number(int n)
 {
+	int j = 0;
 	int orig = n;
 	unsigned int i = 0;
 
+	if (n == -2147483648)
+	{
+		n += 1;
+		orig += 1;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
+		orig = -orig;
 	}
 	if (n == 0)
 		_putchar('0');
-	for (; n != 0; n = n / 10)
-		i = i * 10 + n % 10;
-	for (; i != 0; i = i / 10)
-		_putchar(i % 10 + '0');
-	if (orig % 10 == 0 && orig != 0)
-		_putchar('0');
+	if (n > 0)
+	{
+		j = n % 10;
+		if (n >= 214748364)
+			n = n / 10;
+		for (; n != 0; n = n / 10)
+			i = i * 10 + n % 10;
+		for (; i != 0; i = i / 10)
+			_putchar(i % 10 + '0');
+		if (orig % 10 == 0 && orig != 0)
+			_putchar('0');
+		if (orig >= 214748364)
+			_putchar(j + '0');
+	}
 }
