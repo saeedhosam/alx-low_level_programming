873927c82de1c224956d7630fd9a2a6a41f01e1d
saeedhosam <makymaky757@gmail.com>
2023-08-29 11:03:36 +0300

    2.5

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char *s = "";
+	char *s = "hello";
 	char *f;
 
-	f = _strchr(s, ' ');
+	f = _strchr(s, 'l');
 
 	if (f != NULL)
 	{
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,19 +11,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *str;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		if (s[i] == c)
 		{
-			str = &s[i];
+			return (&s[i]);
 			break;
 		}
-		else
-			str = '\0';
-	}
-	if (s[0] == '\0')
-		str = '\0';
-	return (str);
+	return ('\0');
 }
