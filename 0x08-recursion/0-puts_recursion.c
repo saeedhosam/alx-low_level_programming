8dc6d766ee3f2d32b8a01f31384e903faf998e33
saeedhosam <makymaky757@gmail.com>
2023-08-30 16:01:29 +0300

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: the string to be printed
+ * Return: notion
+*/
+void _puts_recursion(char *s)
+{
+	_putchar(s[0]);
+	if (s[0] == 'n')
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s + 1);
+}
