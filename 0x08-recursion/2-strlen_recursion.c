e53dc43761127bc91f2912c0985ff6fa9ae37dc5
saeedhosam <makymaky757@gmail.com>
2023-08-31 06:51:36 +0300

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,12 @@
 /**
  * _strlen_recursion -  returns the length of a string.
  * @s: the string to be printed
- * Return: int
+ * Return: nothing
 */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-
-	_putchar(*s);
-	_strlen_recursion(s + 1);
+	if (s[0] == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
 }
