849efc38a31b6316c9c6edde0bc33243095ffe05
saeedhosam <makymaky757@gmail.com>
2023-09-01 02:40:34 +0300

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
+
+/**
+ * counting - gives the string length
+ * @s: the string
+ * Return: integer.
+*/
+int counting(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	else
+		return (counting(s + 1) + 1);
+}
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not
  * @s: the string to be tested
@@ -8,10 +21,13 @@
 */
 int is_palindrome(char *s)
 {
-	char str[100];
-	
-    if (s[0] == '\0')
-        return (0);
-    else
-        return (is_palindrome(s + 1) + 1);
+	int n;
+
+	n = counting(s);
+	if (s[0] == s[n - 1]
+	&& s[1] == s[n - 2]
+	&& s[2] == s[n - 3])
+		return (1);
+	else
+		return (0);
 }
