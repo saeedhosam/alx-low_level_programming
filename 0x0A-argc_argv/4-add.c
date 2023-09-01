260bbe54609aa52851b90d02668ea4ed113729fd
saeedhosam <makymaky757@gmail.com>
2023-09-02 00:55:34 +0300

    4.4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,11 +21,12 @@ int main(int argc, char *argv[])
 		char *c = argv[argc - 1];
 
 		if (argc > 1)
-			if (c[0] < 48 || c[0] > 57)
-			{
-				printf("Error\n");
-				return (1);
-			}
+			if (c[0] != '-')
+				if (c[0] < 48 || c[0] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 		i = i + atoi(argv[argc - 1]);
 	}
 	printf("%d\n", i);
