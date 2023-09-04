52a6f72713f4386e080f500f90288e8d315458f0
saeedhosam <makymaky757@gmail.com>
2023-09-04 21:43:30 +0300

    1.1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,9 +13,13 @@ char *_strdup(char *str)
 	int i;
 	char *ptr;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0';)
 		i++;
 	ptr = malloc(sizeof(char) * (i + 1));
+		if (ptr == NULL)
+			return (NULL);
 	strcpy(ptr, str);
 	return (ptr);
 }
