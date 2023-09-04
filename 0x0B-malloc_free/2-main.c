a27a8f7c696327c90604aaee7105a41063578665
saeedhosam <makymaky757@gmail.com>
2023-09-04 21:55:23 +0300

    2

---
+++ 0x0B-malloc_free/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s;
+
+    s = str_concat("Betty ", "Holberton");
+    printf("%s\n", s);
+    return (0);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: string.
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2;
+	char *ptr;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	ptr = malloc(sizeof(char) * len1 + sizeof(char) * len2);
+	if (ptr == NULL)
+		return (NULL);
+	strcat(ptr, s1);
+	strcat(ptr, s2);
+	return (ptr);
+}
