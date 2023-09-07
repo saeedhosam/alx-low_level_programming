8fa6530d34466d0c95d46a49617e91438957312c
saeedhosam <makymaky757@gmail.com>
2023-09-07 23:46:51 +0300

    2.3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,11 +15,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ptr = malloc(sizeof(size) * nmemb);
+		return NULL;
+
+	if (nmemb > UINT_MAX / size) // check for overflow
+		return NULL;
+
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-		return (NULL);
+		return NULL;
+
 	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
-	return (ptr);
+
+	return ptr;
 }
