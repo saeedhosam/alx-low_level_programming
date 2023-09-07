7f5323424abe969d7e0996f995bc8446adc67f60
saeedhosam <makymaky757@gmail.com>
2023-09-08 00:23:29 +0300

    2.15

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,8 +12,7 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *ptr;
+	void *ptr;
 
 	if (nmemb <= 0 || size == 0)
 		return (NULL);
@@ -22,8 +21,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
-
+	memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
