96f938ab9bc59d9872cd7735075eaac35054c53e
saeedhosam <makymaky757@gmail.com>
2023-10-15 17:59:41 +0300

    10.1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,6 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		free(cn);
 		return (1);
 	}
 
@@ -29,6 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (i == index)
 		{
 			behind->next = cn->next;
+			free(cn);
 			return (1);
 		}
 
